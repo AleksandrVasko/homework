18128eb022c3d255893ec50b4a844db28a900aff
Alexandr Vasko <alexandervasko11@gmail.com>
2022-05-30 14:14:20 +0700

    Выполнил дз по второму уроку

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-let title = 'title';
-let screens = 'screens';
-let screenPrice = 'screenPrice';
-let rollback = 'rollback';
-let fullPrice = 'fullPrice';
-let adaptive = 'adaptive';
+let title = 'New project';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 100;
+let rollback = 50;
+let fullPrice = 100;
+let adaptive = true;
 
-alert('Hello world!');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
-console.log('Hello world!');
+console.log(screens.length);
+
+console.log('Стоимость верстки экранов' + ' ' + (screenPrice) + ' ' + 'рублей/1,51 долларов/44,37 гривен/9,94 юани.');
+console.log('Стоимость разработки сайта' + ' ' + (fullPrice) + ' ' + 'рублей/1,51 долларов/44,37 гривен/9,94 юани.');
+
+screens = screens.toLowerCase();
+screens = screens.split(' ');
+console.log(screens);
+
+console.log(fullPrice * (rollback/100));
