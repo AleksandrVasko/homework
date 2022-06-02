6ebc3b82df227a1791b6ef0a9f008927504da719
Alexandr Vasko <alexandervasko11@gmail.com>
2022-06-02 17:58:54 +0700

    Исправил задание по третьему уроку второй раз

--- script.js
+++ script.js
@@ -1,37 +1,14 @@
-//lesson02
-let title = 'New project';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 100;
-let rollback = 50;
-let fullPrice = 100;
-let adaptive = true;
-
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-
-console.log(screens.length);
-
-console.log('Стоимость верстки экранов' + ' ' + (screenPrice) + ' ' + 'рублей/1,51 долларов/44,37 гривен/9,94 юани.');
-console.log('Стоимость разработки сайта' + ' ' + (fullPrice) + ' ' + 'рублей/1,51 долларов/44,37 гривен/9,94 юани.');
-
-screens = screens.toLowerCase().split(' ');
-console.log(screens);
-
-console.log(fullPrice * (rollback/100));
-
-
 //3
-title = prompt('Как называется ваш проект?');
+const title = prompt('Как называется ваш проект?');
 
 //4
-screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
 
 //5
-screenPrice = prompt('Сколько будет стоить данная работа?', '12000');
+const screenPrice = prompt('Сколько будет стоить данная работа?', '12000');
 
 //6
-adaptive = confirm('Нужен ли адаптив на сайте?');
+const adaptive = confirm('Нужен ли адаптив на сайте?');
 
 //7
 const service1 = prompt('Какой дополнительный тип услуги нужен?');
@@ -40,10 +17,10 @@ const service2 = prompt('Какой дополнительный тип услу
 const servicePrice2 = prompt('Сколько это будет стоить?');
 
 //8
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
 //9
-rollback = 50;
+const rollback = 50;
 let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
 console.log(servicePercentPrice);
 
@@ -65,3 +42,20 @@ switch (true) {
     default:
         break;
 }
+
+
+
+//lesson02
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log('Стоимость верстки экранов' + ' ' + (screenPrice) + ' ' + 'рублей');
+console.log('Стоимость разработки сайта' + ' ' + (fullPrice) + ' ' + 'рублей');
+
+screens = screens.toLowerCase().split(' ');
+console.log(screens);
+
+console.log(fullPrice * (rollback/100));
