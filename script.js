90b75271a08dad05bed404a8a57c4fa80c48b282
Alexandr Vasko <alexandervasko11@gmail.com>
2022-06-05 13:16:55 +0700

    окончательный вариант задания по lesson04

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use sctrict';
 
-const title = prompt('Как называется ваш проект?');
+let title = prompt('Как называется ваш проект?' );
 let screens = prompt('Какие типы экранов нужно разработать?');
 const screenPrice = +prompt('Сколько будет стоить данная работа?');
 const adaptive = confirm('Нужен ли адаптив на сайте?');
@@ -9,9 +9,8 @@ const servicePrice1 = +prompt('Сколько это будет стоить?');
 const service2 = prompt('Какой дополнительный тип услуги нужен?');
 const servicePrice2 = +prompt('Сколько это будет стоить?');
 const rollback = 50;
-
+let fullPrice = 0;
 let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
-
 
 const getRollbackMessage = function (price) {
     if (price >= 30000) {
@@ -28,24 +27,32 @@ const getRollbackMessage = function (price) {
 const showTypeOf = function (variable) {
     console.log(variable, typeof variable);
 };
-
 const getAllServicePrices = function () {
     return servicePrice1 + servicePrice2;
 };
-
+const getServicePercentPrice = function () {
+    return (fullPrice - (fullPrice * (rollback / 100)));
+};
+const getTitle = function () {
+    return title[0].toUpperCase() + title.slice(1).toLowerCase();
+};
 function getFullPrice() {
     return screenPrice + allServicePrices;
 }
-fullPrice = getFullPrice();
+
 const allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+title = getTitle();
+servicePercentPrice = getServicePercentPrice();
+
 
 showTypeOf(title);
 showTypeOf(screenPrice);
 showTypeOf(adaptive);
-
+console.log(screens);
 console.log(getRollbackMessage(fullPrice));
-console.log(servicePercentPrice);
-console.log('Стоимость верстки экранов' + ' ' + (screenPrice) + ' ' + 'рублей');
+console.log(getServicePercentPrice);
+
 
 
 
