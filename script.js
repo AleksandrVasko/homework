04573bb1bf41a71ca9b40d4eacdb23073f08fa07
Alexandr Vasko <alexandervasko11@gmail.com>
2022-06-16 22:09:17 +0700

    выполнил урок 9, добавил вызов через DOM, создал ветку lesson09

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 'use sctrict';
 
+let mainHeader = document.getElementsByTagName("h1")[0];
+let heandlerBtn1 = document.getElementsByClassName("handler_btn")[0];
+let heandlerBtn2 = document.getElementsByClassName("handler_btn")[1];
+let screenBtn = document.querySelector(".screen-btn");
+let otherItemsPercent = document.querySelectorAll(".percent.other-items ");
+let otherItemsNumber = document.querySelectorAll(".number.other-items");
+let inputRange = document.querySelector(".rollback>input");
+let span = document.querySelector(".rollback>span");
+let inputs = document.getElementsByClassName("total-input");
+let screensBlocks = document.querySelectorAll("screen");
+
 const appData = {
     title: '',
     screens: [],
